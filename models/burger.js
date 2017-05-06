39f089dd598e4f2fcab26d28c6ca74c26dcf8f7e
Danny Kim <dannyjwkim@Daniels-MacBook-Pro.local>
2017-05-06 09:17:58 -0400

    HW MVP Done

---
+++ models/burger.js
@@ -0,0 +1,37 @@
+var orm = require('../config/orm.js');
+
+var burger = {
+
+  selectWhere: function(burgerName,callback){
+    orm.selectWhere('burgers','burger_name' ,burgerName,function(returnData){
+      callback(returnData)
+    }); 
+  },
+
+  insertOne: function(column,name,callback){
+    orm.insertOne('burgers',column,name, function(returnData){
+      callback(returnData);
+    });
+  },
+
+  selectAll: function(callback){
+    orm.selectAll('burgers',function(returnData){
+      callback(returnData);
+    });
+  },
+
+  updateOne: function(table,column,id,callback){
+    orm.updateOne('burgers',column, id,function(returnData){
+      callback(returnData);
+    })
+  },
+
+  deleteOne: function(table,id,callback){
+    orm.deleteOne('burgers',id,function(returnData){
+      callback(returnData);
+    })
+  }
+
+}
+
+module.exports = burger;
--- server.js
+++ server.js
@@ -5,6 +5,31 @@ var methodOverride = require("method-override");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+//serve up public folder and all content as static files to server.
+app.use(express.static('public'));
+
+//use bodyParser, do not encode url
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+//require handlebars
+var exphbs = require('express-handlebars');
+
+//use handlebars engine as template engine, use 'main' as our base file
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+//link to burger controller, set as default page"/"
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
 app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
 });
